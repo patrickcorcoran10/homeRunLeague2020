32fe959cc3f5574f6994aa2761aa2b9b7e912802
Patty C <patrickcorcoran10@gmail.com>
2020-04-14 18:10:58 -0500

    commit to deploy

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,11 +4,11 @@ const Momentjs = require("moment");
 
 const jwt = require("jsonwebtoken");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Using the passport.authenticate middleware with our local strategy.
   // If the user has valid login credentials, send them to the members page.
   // Otherwise the user will be sent an error
-  app.post("/api/login", passport.authenticate("local"), function(req, res) {
+  app.post("/api/login", passport.authenticate("local"), function (req, res) {
     // Since we're doing a POST with javascript, we can't actually redirect that post into a GET request
     // So we're sending the user back the route to the members page because the redirect will happen on the front end
     // They won't get this or even be able to access this page if they aren't authed
@@ -19,13 +19,13 @@ module.exports = function(app) {
       if (err) {
         return res.status(422).json(err.errors[0].message);
       }
-      req.login(user, { session: false }, err => {
+      req.login(user, { session: false }, (err) => {
         if (err) {
           res.send(err);
         }
         const token = jwt.sign(user.id, process.env.JWT_SECRET);
         return res.json({
-          token
+          token,
         });
       });
     })(req, res);
@@ -36,19 +36,19 @@ module.exports = function(app) {
   // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
   // otherwise send back an error
-  app.post("/api/register", function(req, res) {
+  app.post("/api/register", function (req, res) {
     console.log(req.body);
     db.User.create({
       // name: req.body.name,
       email: req.body.email,
-      password: req.body.password
+      password: req.body.password,
     })
-      .then(function() {
+      .then(function () {
         window.location.href = "/login";
 
         // res.redirect(307, "/login");
       })
-      .catch(function(err) {
+      .catch(function (err) {
         console.log(err);
         res.json(err);
         // res.status(422).json(err.errors[0].message);
@@ -56,7 +56,7 @@ module.exports = function(app) {
   });
 
   // Route for logging user out
-  app.post("/api/logout", function(req, res) {
+  app.post("/api/logout", function (req, res) {
     req.logout();
     res.redirect("/");
   });
@@ -89,15 +89,13 @@ module.exports = function(app) {
       junePickTotal: 0,
       julyPickTotal: 0,
       augustPickTotal: 0,
-      septemberPickTotal: 0
-    }).then(dbData => {
+      septemberPickTotal: 0,
+    }).then((dbData) => {
       res.json(dbData);
     });
   });
   // GET Route for Admin Page April
-  let month = Momentjs()
-    .format("MMMM")
-    .toLowerCase();
+  let month = Momentjs().format("MMMM").toLowerCase();
   console.log(month);
   let monthNum = Momentjs().format("M");
   console.log(monthNum);
@@ -110,9 +108,9 @@ module.exports = function(app) {
     db.Drafts.findAll({
       where: {
         cut: false,
-        month: monthNum
-      }
-    }).then(dbData => {
+        month: monthNum,
+      },
+    }).then((dbData) => {
       res.json(dbData);
     });
   });
@@ -121,9 +119,9 @@ module.exports = function(app) {
     db.Drafts.findAll({
       where: {
         cut: false,
-        month: monthNum
-      }
-    }).then(dbData => {
+        month: monthNum,
+      },
+    }).then((dbData) => {
       res.json(dbData);
     });
   });
@@ -132,9 +130,9 @@ module.exports = function(app) {
     db.Drafts.findAll({
       where: {
         cut: false,
-        month: req.params.month
-      }
-    }).then(dbData => {
+        month: req.params.month,
+      },
+    }).then((dbData) => {
       res.json(dbData);
     });
   });
@@ -142,9 +140,9 @@ module.exports = function(app) {
   app.delete("/api/delete:id", (req, res) => {
     db.Drafts.destroy({
       where: {
-        id: req.params.id
-      }
-    }).then(dbData => {
+        id: req.params.id,
+      },
+    }).then((dbData) => {
       res.json(dbData);
     });
   });
@@ -152,14 +150,14 @@ module.exports = function(app) {
   app.put("/api/cut-player:id", (req, res) => {
     db.Drafts.update(
       {
-        cut: true
+        cut: true,
       },
       {
         where: {
-          id: req.params.id
-        }
+          id: req.params.id,
+        },
       }
-    ).then(dbData => {
+    ).then((dbData) => {
       res.json(dbData);
     });
   });
@@ -168,15 +166,15 @@ module.exports = function(app) {
     db.Drafts.update(
       {
         total: req.body.total,
-        month: req.body.month + 1
+        month: req.body.month + 1,
         // total: "35"
       },
       {
         where: {
-          pickID: req.params.pickID
-        }
+          pickID: req.params.pickID,
+        },
       }
-    ).then(dbData => {
+    ).then((dbData) => {
       res.json(dbData);
     });
   });
